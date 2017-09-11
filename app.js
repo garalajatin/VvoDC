22e084c5cf1cd384db1423c5ed3229f0b34dcb99
zeitech-74 <zeitech-74@zeitech-74-PC>
2017-09-11 12:47:29 +0530

    Harshit structure changed

--- app.js
+++ app.js
@@ -1,72 +1,31 @@
 'use strict';
 
-var app = angular.module('StarterApp', ['ngMaterial', 'ngMdIcons', 'ngRoute']);
+var app = angular.module('StarterApp', [ 'ngMaterial', 'ngMdIcons', 'ngRoute' ]);
 
 
-app.config(['$locationProvider', function($locationProvider) {
-  $locationProvider.hashPrefix('');
-}]);
+app.config([ '$locationProvider', function($locationProvider) {
+	$locationProvider.hashPrefix('');
+} ]);
 
-app.config(['$routeProvider', 
+app.config([ '$routeProvider',
 
-  function( $routeProvider) {
+	function($routeProvider) {
 
-  // Define routes 
-  $routeProvider.
-    when('/login', 
-        { templateUrl: 'partial/login.html',
-        	controller: 'loginCtrl'	
-        }).
-
-    when('/dashboard', 
-        { templateUrl: 'partial/dashboard.html',
-    	controller: 'dashboardCtrl'	
-        }).
-
-    
-     when('/contacts',
-        { templateUrl: 'partial/contacts.html'}).
-
-    otherwise({redirectTo: 'login'});
+		// Define routes 
+		$routeProvider.when('/login',
+			{
+				templateUrl : 'src/vvo/login/login.html',
+				controller : 'loginCtrl'
+			}).when('/dashboard',
+			{
+				templateUrl : 'src/vvo/dashboard/dashboard.html',
+				controller : 'dashboardCtrl'
+			}).when('/contacts',
+			{
+				templateUrl : 'partial/contacts.html'
+			}).otherwise({
+			redirectTo : 'login'
+		});
 
 
-  }]);
-
-
-
-app.controller('loginCtrl',  function($http, $location){
-	
-	  var lCtrl = this;
-	  lCtrl.formData = {
-		  'loginName' : '',
-		  'password': ''
-	  };
-	  
-	  lCtrl.errorMessage = undefined;
-	  lCtrl.onClickLink = function(department) {
-			
-		};
-	  lCtrl.onLogin = function() {
-		  
-		  $location.path('/dashboard');
-	  }
-	  
-	});
-
-app.controller('dashboardCtrl', function ($scope, $mdSidenav) {
-    $scope.toShow = "home";
-  
-    $scope.toggleLeft = function() {
-        $mdSidenav("left")
-          .toggle();
-    };
-    
-    $scope.close = function () {
-      $mdSidenav('left').close();
-    };
-
-    $scope.show = function (toShow) {
-      $scope.toShow = toShow;
-    };
-});
-
+	} ]);
